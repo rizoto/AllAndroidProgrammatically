0a127cebe14803941c5eeadd2c046e5e82a29f46
Lubor Kolacny <lkolacny@me.com>
2015-05-07 06:31:10 +1000

    Adding Networking

--- app/src/main/java/com/gpt/dynlayout/MainActivity.java
+++ app/src/main/java/com/gpt/dynlayout/MainActivity.java
@@ -1,17 +1,40 @@
 package com.gpt.dynlayout;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.Gravity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 import android.view.ViewGroup.*;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView resultTextView;
+
+    public TextView getResultTextView() {
+        return resultTextView;
+    }
+
+    public void setResultTextView(TextView resultTextView) {
+        this.resultTextView = resultTextView;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,10 +54,26 @@ public class MainActivity extends AppCompatActivity {
         tv.setText("TextView");
         tv.setLayoutParams(lpView);
         linLayout.addView(tv);
+        setResultTextView(tv);
 
         Button btn = new Button(this);
         btn.setText("Button");
         linLayout.addView(btn, lpView);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String stringUrl = "http://www.google.com";
+                ConnectivityManager connMgr = (ConnectivityManager)
+                        getSystemService(Context.CONNECTIVITY_SERVICE);
+                NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+                if (networkInfo != null && networkInfo.isConnected()) {
+                    new DownloadTask().execute(stringUrl);
+                } else {
+                    Toast.makeText(getApplicationContext(), "No network connection available.", Toast.LENGTH_SHORT);
+                }
+            }
+        });
 
 
         LinearLayout.LayoutParams leftMarginParams = new LinearLayout.LayoutParams(
@@ -56,6 +95,67 @@ public class MainActivity extends AppCompatActivity {
 
         getSupportActionBar().setTitle("My Activity");
     }
+
+    private class DownloadTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                return download(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            // textView.setText(result);
+            getResultTextView().setText(result);
+        }
+        private static final String DEBUG_TAG = "Http";
+        // Given a URL, establishes an HttpUrlConnection and retrieves
+        // content as a InputStream, which it returns as
+        // a string.
+        private String download(String myUrl) throws IOException {
+            InputStream is = null;
+            int len = 500;
+
+            try {
+                URL url = new URL(myUrl);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                conn.setReadTimeout(10000 /* milliseconds */);
+                conn.setConnectTimeout(15000 /* milliseconds */);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                // Starts the query
+                conn.connect();
+                int response = conn.getResponseCode();
+                Log.d(DEBUG_TAG, "The response is: " + response);
+                is = conn.getInputStream();
+
+                // Convert the InputStream into a string
+                String contentAsString = readIt(is, len);
+                return contentAsString;
+
+                // Makes sure that the InputStream is closed after the app is
+                // finished using it.
+            } finally {
+                if (is != null) {
+                    is.close();
+                }
+            }
+        }
+        // Reads an InputStream and converts it to a String.
+        String readIt(InputStream stream, int len) throws IOException, UnsupportedEncodingException {
+            Reader reader = null;
+            reader = new InputStreamReader(stream, "UTF-8");
+            char[] buffer = new char[len];
+            reader.read(buffer);
+            return new String(buffer);
+        }
+    }
+
 
 //    @Override
 //    public boolean onCreateOptionsMenu(Menu menu) {
