1e7d5319897802024b5b7618713b6c7e80c4ccfd
Lubor Kolacny <lkolacny@me.com>
2015-05-07 23:50:33 +1000

    adding google map support, change to do it in the code

--- app/src/main/java/com/gpt/dynlayout/MapActivity.java
+++ app/src/main/java/com/gpt/dynlayout/MapActivity.java
@@ -1,9 +1,11 @@
 package com.gpt.dynlayout;
 
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.maps.SupportMapFragment;
 
 
 public class MapActivity extends FragmentActivity {
@@ -11,29 +13,11 @@ public class MapActivity extends FragmentActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_map, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+        //RelativeLayout relativeLayout = new RelativeLayout(this);
+        if(savedInstanceState == null) {
+            SupportMapFragment mapFragment = SupportMapFragment.newInstance();
+            getSupportFragmentManager().beginTransaction().add(android.R.id.content,mapFragment).commit();
         }
-
-        return super.onOptionsItemSelected(item);
     }
+
 }
